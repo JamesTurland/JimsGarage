a2c1285615a29b3a59275ae349c28bce954538d7
Trever Ehrfurth <trever.ehrfurth@gmail.com>
2024-05-07 00:42:10 -0500

    Update k3s.sh
    update Kube-Vip and k3s versions

--- Kubernetes/K3S-Deploy/k3s.sh
+++ Kubernetes/K3S-Deploy/k3s.sh
@@ -21,10 +21,10 @@ echo -e " \033[32;5m                                                           \
 #############################################
 
 # Version of Kube-VIP to deploy
-KVVERSION="v0.6.3"
+KVVERSION="v0.8.0"
 
 # K3S Version
-k3sVersion="v1.26.10+k3s2"
+k3sVersion="v1.29.4+k3s1"
 
 # Set the IP addresses of the master and work nodes
 master1=192.168.3.21
